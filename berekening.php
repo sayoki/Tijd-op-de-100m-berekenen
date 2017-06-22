435e7635d9d619ad235a480951d700fa43a1251b
Sjors Thielen <sjorsthielen@gmail.com>
2017-06-22 18:37:07 +0200

    Berekening

---
+++ berekening.php
@@ -0,0 +1,12 @@
+<?php
+$tempo = "3.15"; # min.seconden
+$afstand = 1000; # aantal meter
+$stukken = $afstand / 100;
+if(preg_match("/(\d+)\.(\d+)/", $tempo, $split)==true) {
+    $seconden = (int) $split[2] + ((int)$split[1] * 60);    
+}
+else {
+    $seconden = (int) $tempo;
+}
+echo $seconden / $stukken;
+?>
