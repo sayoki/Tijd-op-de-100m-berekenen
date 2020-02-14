829d79f4620d2f26cc7dfce1831a030229f4907b
Sjors <sjorsthielen@gmail.com>
2020-02-14 14:31:56 +0100

    Update berekening.php

--- berekening.php
+++ berekening.php
@@ -1,6 +1,9 @@
 <?php
 $tempo = "3.15"; # min.seconden
 $afstand = 1000; # aantal meter
+
+###
+
 $stukken = $afstand / 100;
 if(preg_match("/(\d+)\.(\d+)/", $tempo, $split)==true) {
     $seconden = (int) $split[2] + ((int)$split[1] * 60);    
